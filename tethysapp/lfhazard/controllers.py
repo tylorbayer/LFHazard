94cf5eafdfbd2260dfacae91704b82fdef3529b9
kevin9kai <kevin9kai@gmail.com>
2017-02-16 11:09:41 -0700

    First commit

--- tethysapp/lfhazard/controllers.py
+++ tethysapp/lfhazard/controllers.py
@@ -10,3 +10,17 @@ def home(request):
     context = {}
 
     return render(request, 'lfhazard/home.html', context)
+def map(request):
+    """
+    Controller for the map
+    """
+    map_options = MapView(height='500px',
+                          width='100%',
+                          layers=[geojson_layer],
+                          view=view_options,
+                          basemap='OpenStreetMap',
+                          legend=True)
+    # Pass variables to the template via the context dictionary
+    context = {'map_options': map_options}
+
+    return render(request, 'lfhazard/map.html', context)
